473535f35c81de1636fbb114cae73b462ce2c5a3
Douglas Back <douglas@bluestatedigital.com>
2013-03-29 09:23:19 -0400

    modernizr conditionally loads the stickem

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -10,19 +10,5 @@
         }
     }
     $(window).scroll(function(){navPinner();}).resize(function(){navPinner()});
-    $('body').stickem({
-        item: '.stickem',
-        container: '.stickem-container',
-        stickClass: 'stickit',
-        endStickClass: 'stickit-end',
-        offset: 0,
-        onStick: function(stick){
-            var $el = $(stick.$elem[0]);
-            $el.css('width', $el.parent().css('width'));
-        },
-        onUnstick: function(el){
-            var $el = $(stick.$elem[0]);
-            $el.css('width', 'auto');
-        }
-    });
+    
 })(jQuery, window)
