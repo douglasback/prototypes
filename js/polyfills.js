6d9693db5b7ae1778c40a7623d265e776cbfa16f
Douglas Back <douglas@bluestatedigital.com>
2013-04-02 10:25:59 -0400

    bunch of changes based on group meeting

--- js/polyfills.js
+++ js/polyfills.js
@@ -9,7 +9,7 @@ Modernizr.load([
                 container: '.stickem-container',
                 stickClass: 'stickit',
                 endStickClass: 'stickit-end',
-                offset: 0,
+                offset: 62,
                 onStick: function(stick){
                     var $el = $(stick.$elem[0]);
                     $el.css('width', $el.parent().css('width'));
